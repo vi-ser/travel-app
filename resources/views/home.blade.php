7e31902133cef8bacb49add56abd4a8e979ec8f2
vi_ser <vinceulisse@gmail.com>
2024-08-24 20:21:49 +0200

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 
-
+First commit!
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
 
     @vite('resources/js/app.js')
 </head>
 
 <body>
 
+    @yield('content')
 
 </body>
 
